09343ecaef2aa7b8f025050a88e361b5b5f832a1
Konstantin-Filipov <kosio1234545@gmail.com>
2023-03-19 06:07:32 +0100

    userID&pass check, redirect view for each role

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const jwt = require("jsonwebtoken")
 var path = require("path")
+const db = require('./database')
 require("dotenv").config()
 
 app.use(express.urlencoded({extended: false}))
@@ -11,23 +12,45 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 var currentKey = ""
-var currentPass = ""
+var payload = ""
+var username = ""
 
 app.get('/', (req,res) => {
     res.redirect("./identify")
 });
 
 app.post('/identify' , (req,res) => {
-    //auth
-    const username = req.body.password
-    const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
-    currentKey = token
-    currentPass = username
-    res.redirect("./granted")
-});
+    
+    const {userID} = req.body
+    const {password} = req.body
+    
+    
+    db.get('SELECT password FROM users WHERE userID = ?', [userID], async (err, row) => 
+    {
+        if (err) 
+        {
+            console.error(err.message);
+        } 
+        else if (!row) 
+        {
+            console.error('User not found');
+            res.render("fail.ejs")
+        }
+        else if (row.password != password)
+        {
+            console.log(row.password)
+            res.render("fail.ejs")
+        }
+        else 
+        {
+            //store them in global vars
+            username = userID
+            payload = {"username": userID, "password": password};
+            currentKey = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET)
 
-app.get('/identify',(req,res) => {
-    res.render("identify.ejs")
+            res.redirect("./granted")
+        } 
+    });
 });
 
 function authenticateToken(req, res, next){
@@ -41,8 +64,45 @@ function authenticateToken(req, res, next){
     }
 }
 
+app.get('/identify',(req,res) => {
+    res.render("identify.ejs")
+});
+
 app.get('/granted', authenticateToken,(req,res)=>{
-    res.render("start.ejs")
+    if (username == "id1")
+    {res.redirect("./student1")}
+
+    else if(username == "id2") 
+    {res.redirect("./student2")}
+    
+    else if(username == "id3")
+    {res.redirect("./teacher")}
+    
+    else if(username == "admin")
+    {res.redirect("./admin")}
+});
+
+app.get('/student1', authenticateToken, (req,res) => {
+    res.render("student1.ejs")
+});
+
+app.get('/student2', authenticateToken, (req,res) => {
+    res.render("student2.ejs")
+});
+
+app.get('/teacher', authenticateToken, (req,res) => {
+    res.render("teacher.ejs")
+});
+
+app.get('/admin', authenticateToken, (req,res) => {
+    db.all('SELECT * FROM users', (err, rows) => {
+        if (err) {
+          console.error(err.message);
+          res.render('fail.ejs', { message: 'An error occurred while fetching user data' });
+        } else {
+          res.render('admin.ejs', { users: rows });
+        }
+    });
 });
 
 app.listen(5000, function(){
